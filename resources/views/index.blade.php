e3c8dabdd9605cd4dbb0144a02431acd15961b08
omarmhd <omarmhd19988@gmail.com>
2026-01-07 13:03:29 +0200

    edit map

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{$event->title_en}} - Registration</title>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
