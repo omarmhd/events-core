ff8067395e9f3be6d5a0f8904c6ec19614b3188f
omarmhd <omarmhd19988@gmail.com>
2026-01-15 09:56:33 +0200

    remove background from logo and title

--- resources/views/emails/tickets.blade.php
+++ resources/views/emails/tickets.blade.php
@@ -50,17 +50,9 @@
             <td align="center" style="padding-bottom: 40px;">
 
                 <div style="margin-bottom: 35px;">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 327.46 134.25" style="width: 160px; max-width: 100%; height: auto; display: inline-block;">
-                        <defs><style>.cls-1{fill:#224d59;}.cls-2{fill:#63ae45;}</style></defs>
-                        <g>
-                            <polygon class="cls-1" points="148.89 80.33 120.48 80.33 107.5 96.5 157.64 96.5 162.73 105.42 185.43 105.42 147.07 34.58 133.51 51.47 148.89 80.33"/>
-                            <polygon class="cls-1" points="297.5 34.58 297.5 34.58 272.73 34.58 244.71 72.75 216.46 34.58 191.63 34.58 191.6 34.58 191.6 105.42 211.56 105.42 211.56 61.51 244.77 106.4 277.68 61.58 277.68 105.42 297.64 105.42 297.64 34.58 297.5 34.58"/>
-                            <rect class="cls-1" x="307.5" y="34.58" width="19.96" height="70.85"/>
-                            <polygon class="cls-2" points="118.2 34.58 65.09 134.25 67.05 134.25 147.07 34.58 118.2 34.58"/>
-                            <polygon class="cls-2" points="149.8 0 124.42 25.37 149.8 25.37 149.8 0"/>
-                            <path class="cls-1" d="M83.39,53.85v-19.27H15.8c-5.59,.17-8.31,1.77-11.17,4.62-2.86,2.86-4.63,6.81-4.63,11.17,0,8.86,6.26,12.66,13,15.99l40.38,19.95H.54v19.11H70.69l13.29-24.95L27.33,53.85h56.06Z"/>
-                        </g>
-                    </svg>
+                    <div style="margin-bottom: 35px;">
+                        <img src="{{asset("Logo-SAMI.png")}}" alt="Logo-SAMI.png" >
+                    </div>
                 </div>
 
                 <div style="margin-bottom: 25px;">
