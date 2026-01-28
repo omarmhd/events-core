cc8916652859f16654ea33b551ca53b938b82b09
omarmhd <omarmhd19988@gmail.com>
2026-01-28 23:02:08 +0200

    test

--- resources/views/admin/invitations/index.blade.php
+++ resources/views/admin/invitations/index.blade.php
@@ -156,7 +156,7 @@
                                 @endphp
 
 
-
+                                @if($row->status!="pending")
 
                                 <button type="button"
                                         class="btn btn-light btn-sm"
@@ -165,14 +165,14 @@
                                     <i class="fas fa-ticket-alt me-2"></i> Copy Tickets Link
                                 </button>
 
-                                @if($row->status!="pending")
+                                @endif
                                 <button type="button"
                                         class="btn btn-light btn-sm text-dark border shadow-sm me-1"
                                         onclick="copyToClipboard(this, `{{ $whatsappMessage }}`)"
                                         title="Copy Invitation Message">
                                     <i class="fas fa-copy"></i>
                                 </button>
-                                @endif
+
                                 <form action="{{ route('invitations.resend') }}" method="POST" class="d-inline" onsubmit="return confirm('Regenerate ticket?');">
                                     @csrf
                                     <input type="hidden" name="id" value="{{ $row->id }}">
